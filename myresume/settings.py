bf9134c53a9b9ffda6cb18dee752a2c4f42f75c9
drewplette <60942408+drewplette@users.noreply.github.com>
2025-07-14 14:24:36 -0500

    Update settings.py

--- myresume/settings.py
+++ myresume/settings.py
@@ -121,12 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 # Default primary key field type
